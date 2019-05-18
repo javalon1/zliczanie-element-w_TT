757034df444041b7f286db88d0877c202b003029
Tomasz Turkiewicz <tturkiewicz83@gmail.com>
2019-05-18 12:12:36 +0100

    working program

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -1,13 +1,21 @@
 package com.sda.algorytmy;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+public class App {
+    public static void main(String[] args) {
+
+        int[] tablica = new int[]{2, 4, 6, 7, 4, 3, 5, 4, 2, 3, 7, 8, 6};
+
+
+        for (int i = 1; i < 10; i++) {
+            int wynik = 0;
+
+            for(int j = 0; j<tablica.length;j++){
+                if (tablica[j]==i){
+                    wynik++;
+                }
+            }
+            System.out.println("w tablicy jest "+ wynik+" - "+i);
+        }
     }
 }
